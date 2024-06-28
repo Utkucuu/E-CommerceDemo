dcb9d63befd4fe06ba470eb539532bdd8ed4a8c1
Utku BAYRAKTAROĞLU <bayraktarogluutku@gmail.com>
2024-06-29 00:39:37 +0300

    endpoint query error resolved

--- src/app/product-detail/[category]/[id]/[title]/page.tsx
+++ src/app/product-detail/[category]/[id]/[title]/page.tsx
@@ -3,17 +3,37 @@ import { useGetProductByIdQuery } from "@/api";
 import Loading from "@/app/loading";
 import ProductDetail from "@/components/productLayouts/productDetail";
 import { notFound } from "next/navigation";
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function ProductDetailPage({
   params,
 }: {
-  params: { id: string };
+  params: { id: string, title:string };
 }) {
+
+  const router = useRouter() 
+
   const {
     data: product,
     error,
     isLoading,
   } = useGetProductByIdQuery(parseInt(params.id));
+
+
+  useEffect(() => {
+    if (product) {
+      const updatedTitle = product.title
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+      if (updatedTitle !== params.title) {
+        router.replace(
+          `/product-detail/${product.category}/${product.id}/${updatedTitle}`,
+        );
+      }
+    }
+  }, [product, params.title, router]);
+
 
   if (isLoading) return <Loading />;
   if (error) {
